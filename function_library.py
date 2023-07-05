13de47821fbaa0ba7fc746fbaa8039a8940b43f3
jbirkenmaier <127735731+jbirkenmaier@users.noreply.github.com>
2023-07-05 16:33:02 +0200

    update

--- function_library.py
+++ function_library.py
@@ -5,15 +5,30 @@ import matplotlib.pyplot as plt
 class av_data():
     def __init__(self,name,av_velocity,av_torque):
         self.name = name
-        self.av_velocity = av_velocity
         self.av_torque = av_torque
-
-    def chop_small_torque(boundary):
-        pass
-        
-
+        self.av_velocity = av_velocity
+        #self.av_velocity = av_velocity
+        #self.av_torque = av_torque
+    
+    def chop_small_torque(self, boundary):
+        index = 0
+        for element in self.av_torque:
+            if element >= boundary:
+                first_torque = element
+                break
+            index += 1
+        for element in self.av_torque[index:]:
+            if element < boundary:
+                raise Exception('There is an element right of the minimum index that is smaller than the boundary.')
+        self.av_torque = self.av_torque[index:]
+        self.av_velocity = self.av_velocity[index:]
+    
     def find_ext_reduction(self, reference):
-        difference = [1-self.av_torque[i]/reference[i] for i in range(len(reference))]  
+
+        len_of_torque = len(self.av_torque)
+        reference = reference[-len_of_torque:]
+        
+        difference = [1-self.av_torque[i]/reference[i] for i in range(len(self.av_torque))]  #torque and reference index not same
         max_difference = max(difference)
         min_difference = min(difference)
         optimal_velocity = next((self.av_velocity[i] for i in range(len(self.av_velocity)) if 1-self.av_torque[i]/reference[i] == max_difference), None)
@@ -22,7 +37,7 @@ class av_data():
         return max_difference, optimal_velocity, min_difference, min_velocity
         
 
-def read_torque_csv(num_of_datapoints, name_of_reference):
+def read_torque_csv(num_of_datapoints, name_of_reference, minimum_acceptable_torque):
     csv_file_list = []
     data = []
     for element in os.listdir():
@@ -48,21 +63,16 @@ def read_torque_csv(num_of_datapoints, name_of_reference):
             data.append(av_data(element, avg_velocity, avg_torque))
 
     reference = next((element for element in data if element.name == name_of_reference),None)
-
+    
+    for element in data:
+        if element.name != reference.name:
+            element.chop_small_torque(minimum_acceptable_torque)
+    
     for element in data:
         if element.name != reference.name:
             print(element.name, ', maximum reduction to reference: %f%%, at velocity: %.2f 1/min, minimum reduction to reference: %f%%, at velocity: %.2f 1/min'%(element.find_ext_reduction(reference.av_torque)[0]*100,element.find_ext_reduction(reference.av_torque)[1],element.find_ext_reduction(reference.av_torque)[2]*100,element.find_ext_reduction(reference.av_torque)[3]))
 
 
 
-
-    #print(reference.name)
-    #print(reference.av_torque)
-
-'''
-def averaging_over(num_of_datapoints, data):
-    avg_data = [np.sum(torque[i:i+num_of_datapoints])/num_of_datapoints for i in range(0,len_of_file-1,num_of_points)]
-    for i in range(len(num_of_datapoints)):
-'''        
     
     
